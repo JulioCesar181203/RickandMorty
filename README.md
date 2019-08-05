c012625edeb1155082bbd1814d8279448380e113
Julio Cesar <jubslopes1812@hotail.com>
2019-08-05 17:33:18 -0300

    jogo finalizado

---
+++ README.md
@@ -0,0 +1,24 @@
+# JOGO RICK AND MORTY#
+
+
+
+##SOBRE##
+- Esse jogo tem a finalidade do jogador conhecer todos os personagens da série Rick and Morty
+- O jogo nos mostra imagens de cada personagem desta série
+- Abaixo estará o link da API
+- https://rickandmortyapi.com/
+
+
+ ##REGRAS##
+ - O Jogador começa o jogo com 0 pontos 
+ - Ao decorrer da partidas, se o jogador acertar o personagem, o jogador ganha 1 ponto
+ - Se o jogador errar, o jogador perde 1 ponto
+ - Boa sorte!!!!
+ - 
+
+
+###PARTICIPANTES###
+ - Julio
+ - Guilherme
+ - Samara
+ - Hian
---
+++ script.js
@@ -0,0 +1,48 @@
+let botao1 = document.querySelector("#botao1")
+let botao2 = document.querySelector("#botao2")
+let caixaDeTexto = document.querySelector("#texto")
+let image = document.querySelector("img")
+let nome = document.querySelector("h3")
+let contador = document.querySelector("#contador")
+let cont = 0;
+
+pesquisarPersonagem = () =>{
+    let random = Math.ceil(Math.random () *493)
+    fetch(`https://rickandmortyapi.com/api/character/${random}`)
+    .then(function(result){
+        return result.json()
+    })
+    .then(function(data){
+        image.src = data.image;
+        console.log(data.name);
+
+        let personagem = data.name.toUpperCase().toString();
+        console.log(personagem)
+        
+        botao1.onclick = () =>{
+            let input = caixaDeTexto.value.toUpperCase().toString();
+            if(input == personagem){
+                acertou();
+            } else{
+                errou();
+            }
+        }
+    })
+}
+
+
+acertou = () => {
+    cont++;
+    contador.innerHTML = cont;
+    setTimeout(pesquisarPersonagem, 1000);
+}
+
+errou = () => {
+    cont--;
+    contador.innerHTML = cont;
+    setTimeout(pesquisarPersonagem, 1000);
+}
+
+
+botao2.onclick = pesquisarPersonagem
+
